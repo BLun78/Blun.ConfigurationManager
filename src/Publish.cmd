8b253ae404776232795627f4b86d0c15dfb534ea
Bjoern <viper78@gmx.net>
2015-07-27 01:26:12 +0200

    add publish.cmd for nuget and symbolsource.org

---
+++ src/Publish.cmd
@@ -0,0 +1,3 @@
+.\.nuget\nuget.exe push .\Blun.ConfigurationManager\bin\Release\Blun.ConfigurationManager.*.*.*.*.nupkg
+.\.nuget\nuget.exe push .\Blun.ConfigurationManager\bin\Release\Blun.ConfigurationManager.*.*.*.*.symbols.nupkg -Source http://nuget.gw.symbolsource.org/Public/NuGet
+pause
